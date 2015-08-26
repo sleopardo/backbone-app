d2aa21a1ff3432f55ba76c2158359fb27c4f674e
sleopardo <santiagoleopardo@gmail.com>
2015-08-26 20:30:07 -0300

    fix gulp fonts dist

--- frontend-app/gulpfile.js
+++ frontend-app/gulpfile.js
@@ -248,7 +248,7 @@ gulp.task('stylesDist', function() {
 gulp.task('fontsDist', function() {
     mkdirp(src.dist.fonts);
 
-    return gulp.src(src.build.fonts + "*")
+    return gulp.src(src.build.fonts + "*/**")
         .pipe(gulp.dest(src.dist.fonts));
 });
 
